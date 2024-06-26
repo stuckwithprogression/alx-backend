b87bce9b3dacf359a603ffbe6db9e448141063d4
stuckwithprogression <chukwunwemichael@gmail.com>
2024-06-26 19:53:45 +0100

    Queuing System In Js

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,13 @@
+import redis from 'redis';
+
+// Create a Redis client
+const client = redis.createClient();
+
+// Event listener for successful connection
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err.message}`);
+});
